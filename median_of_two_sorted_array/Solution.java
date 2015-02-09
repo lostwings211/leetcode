8c2f93f007ffefec85b422a005792702972fa626
Zhili Hua <zhua@corp.epals.com>
2015-02-09 13:46:48 -0500

    first commit

---
+++ median_of_two_sorted_array/Solution.java
@@ -0,0 +1,55 @@
+public class Solution {
+	public static double findMeidanSortedArrays(int A[], int B[]) {
+		double result;
+		int a_length = (A != null) ? A.length : 0;
+		int b_length = (B != null) ? B.length : 0;
+		int total_length =  a_length + b_length;
+		if (total_length == 0) return 0.0;
+		int[] combined = new int[total_length];
+		int i = 0;
+		int j = 0;
+		int index = 0;
+		
+		while ( i < a_length || j < b_length ) {
+
+			if (i == a_length) {
+				while ( j < b_length) {
+					combined[index++] = B[j++];
+				}
+				break;
+			}
+
+			if(j == b_length) {
+				while ( i < a_length ) {
+					combined[index++] = A[i++];
+				}
+				break;
+			}
+
+			if (A[i] < B[j]) {
+				combined[index] =  A[i];
+				i++;
+			}
+			else {
+				combined[index] = B[j];
+				j++;
+			}
+			index++;
+		} 
+		
+		if (total_length % 2 == 1) {
+			result =  combined[total_length/2];
+		}
+		else {
+			result =  (combined[total_length/2 - 1] + combined[total_length/2]) /  2.0;
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		int[] A = new int[] {1, 3, 5, 7};
+		int[] B = new int[] {2, 4, 6, 8, 10};
+		double result = findMeidanSortedArrays(A, B);
+		System.out.println("Median is :  " + result);
+	}
+}
---
+++ regular_expression_matching/Solution.java
@@ -0,0 +1,85 @@
+public class Solution {
+	public static boolean isMatch(String s, String p) {
+		if(s == null || p == null) return false;
+        if (p.length() == 2) {
+            if (p.charAt(1) == '*') {
+                if(p.charAt(0) == '.') {
+                    return true;
+                }
+                else {
+                    for(int i = 0; i < s.length(); i++) {
+                        if (s.charAt(i) != p.charAt(0)) {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+            }
+        }
+        else if (p.length() == 1) {
+            if(s.length() ==  1) {
+                if (p.charAt(0) == '.') {
+                    return true;
+                }   
+                else {
+                    return (s.charAt(0) == p.charAt(0));
+                }   
+            }   
+            else {
+                return false;
+            }   
+        } 
+        else if (p.length() == 0) {
+            if(s.length() == 0) {
+                return true;
+            }
+            return false;
+        }
+
+        char last_p = p.charAt(p.length() - 1);
+        if(last_p != '*' && last_p != '.' && s.length() > 0) {
+            char last_s =  s.charAt(s.length() - 1);
+            if (last_s !=  last_p) {
+                return false;
+            }
+        }
+
+        for (int i = 0; i <= s.length(); i++) {
+            for (int j = 0; j< p.length()-1; j++) {
+                String front_sub = s.substring(0, i);
+                String back_sub = s.substring(i, s.length());
+                String front_pattern =  p.substring(0,j+1);
+                String back_pattern = p.substring(j+1, p.length());
+   
+                if ((front_pattern.charAt(0) != '*') &&
+                    (back_pattern.charAt(0) != '*')) { 
+                    boolean front_match = isMatch(front_sub, front_pattern);
+                    if(front_match == false) {
+                        break;
+                    }
+                    boolean back_match = isMatch(back_sub, back_pattern);
+                    if (front_match && back_match) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+	}
+	
+	public static void main(String[] args) {
+		if(args.length == 2) {
+			boolean result = isMatch(args[0], args[1]);
+			System.out.print(args[0] + "  " + args[1] + " ");
+			if(result) {
+				System.out.println(" is matched");
+			}
+			else {
+				System.out.println(" is not matched");
+			}  	
+		}
+		else {
+			System.out.println("Invalid Command line arguments " + args.length);
+		}
+	}
+}
