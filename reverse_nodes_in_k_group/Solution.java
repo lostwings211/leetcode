7b6ac9bb80835bff10af18a21b02dfdd142c79de
Zhili Hua <zhua@corp.epals.com>
2015-02-17 18:15:54 -0500

    Solution for public class ListNode {

---
+++ reverse_nodes_in_k_group/Solution.java
@@ -0,0 +1,56 @@
+public class Solution {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode top =  new ListNode(0);
+        top.next = head;
+        ListNode prev = top;
+        ListNode curr = head;
+        ListNode last = getKthNode(head, k);
+        while (curr != null & last !=null) {
+            ListNode temp = last.next;
+            swapNodes(curr, last);
+            
+            prev.next = last;
+            curr.next = temp;
+            prev =  curr;
+            curr = temp;
+            last = getKthNode(curr, k);
+        }
+        return top.next;
+    }
+    
+    public void swapNodes(ListNode head, ListNode tail) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while(curr !=  tail) {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        tail.next = prev;
+    }
+    
+    public ListNode getKthNode(ListNode head, int k) {
+        int count = 1;
+        ListNode curr = head;
+        while(count < k) {
+            if(curr != null) {
+                curr = curr.next;
+            }
+            else {
+                break;
+            }
+            count++;
+        }
+        return curr;
+    }
+}
