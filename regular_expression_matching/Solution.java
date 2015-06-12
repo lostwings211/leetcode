2ba9a6aca0e1a3dc3b0ea7682d1c3243140cb2e3
Zhili Hua <zhua@corp.epals.com>
2015-06-11 23:00:41 -0400

    update regular_expression_matching

--- regular_expression_matching/Solution.java
+++ regular_expression_matching/Solution.java
@@ -1,62 +1,47 @@
 public class Solution {
-    public static boolean isMatch(String s, String p) {
-        if (s == null || p == null)
-            return false;
-        if (p.length() == 2) {
-            if (p.charAt(1) == '*') {
-                if (p.charAt(0) == '.') {
-                    return true;
-                } else {
-                    for (int i = 0; i < s.length(); i++) {
-                        if (s.charAt(i) != p.charAt(0)) {
-                            return false;
-                        }
-                    }
-                    return true;
-                }
-            }
-        } else if (p.length() == 1) {
-            if (s.length() == 1) {
-                if (p.charAt(0) == '.') {
-                    return true;
-                } else {
-                    return (s.charAt(0) == p.charAt(0));
-                }
-            } else {
+    public boolean isMatch(String s, String p) {
+        if(p.length() == 0) {
+            return s.length() == 0;
+        }
+        if(p.length() == 1) {
+            return (s.length() == 1 && (s.charAt(0) ==  p.charAt(0) || p.charAt(0) == '.'));
+        }
+        
+        if(p.charAt(1) !=  '*') {
+            if(s.length() < 1) {
                 return false;
             }
-        } else if (p.length() == 0) {
-            if (s.length() == 0) {
-                return true;
-            }
-            return false;
-        }
-
-        char last_p = p.charAt(p.length() - 1);
-        if (last_p != '*' && last_p != '.' && s.length() > 0) {
-            char last_s = s.charAt(s.length() - 1);
-            if (last_s != last_p) {
+            else if(p.charAt(0) != '.' && s.charAt(0) != p.charAt(0)) {
                 return false;
             }
+            else {
+                return isMatch(s.substring(1), p.substring(1));
+            }
         }
-
-        for (int i = 0; i <= s.length(); i++) {
-            for (int j = 0; j < p.length() - 1; j++) {
-                String front_sub = s.substring(0, i);
-                String back_sub = s.substring(i, s.length());
-                String front_pattern = p.substring(0, j + 1);
-                String back_pattern = p.substring(j + 1, p.length());
-
-                if ((front_pattern.charAt(0) != '*')
-                        && (back_pattern.charAt(0) != '*')) {
-                    boolean front_match = isMatch(front_sub, front_pattern);
-                    if (front_match == false) {
-                        break;
-                    }
-                    boolean back_match = isMatch(back_sub, back_pattern);
-                    if (front_match && back_match) {
+        else if (p.charAt(1) == '*') {
+            if(p.charAt(0) == '.') {
+                for(int i = 0; i <= s.length(); i++) {
+                    boolean isValid = false;
+                    isValid = isMatch(s.substring(i, s.length()), p.substring(2));
+                    if(isValid) {
                         return true;
                     }
+                }
+            }
+            else {
+                boolean isValid = false;
+                isValid = isMatch(s, p.substring(2));
+                if(isValid) {
+                    return true;
+                }
+                int i = 0;
+                while (i < s.length() && s.charAt(i) == p.charAt(0)) {
+                    boolean isValid1 = false;
+                    isValid1 = isMatch(s.substring(i+1, s.length()), p.substring(2));
+                    if(isValid1) {
+                        return true;
+                    }
+                    i++;
                 }
             }
         }
