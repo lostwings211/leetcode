5a25088360060cf40202c5971b85b00841e9efad
Zhili Hua <zhua@corp.epals.com>
2015-06-12 15:59:10 -0400

    modify roman_to_integer

--- roman_to_integer/Solution.java
+++ roman_to_integer/Solution.java
@@ -1,45 +1,26 @@
 public class Solution {
-    public int convert(char c) {
-        switch (c) {
-            case 'I':
-                return 1;
-            case 'V':
-                return 5;
-            case 'X':
-                return 10;
-            case 'L':
-                return 50;
-            case 'C':
-                return 100;
-            case 'D':
-                return 500;
-            case 'M':
-                return 1000; 
-        }
-        return 0;
-    }
-
     public int romanToInt(String s) {
-        int result = 0;
-        char[] char_array = s.toCharArray();
-        int index = 0;
-        int length = char_array.length;
-        while (index < length) {
-            char curr_symbol = char_array[index];
-            int curr_val = convert(curr_symbol);
-            if((curr_symbol == 'I' || curr_symbol == 'X' || curr_symbol == 'C') 
-                && index + 1 < length) {
-                char next_symbol = char_array[index + 1];
-                int next_val = convert(next_symbol);
-                if (curr_val < next_val) {
-                    result += (next_val - curr_val);
-                    index = index + 2;
-                    continue;
-                }   
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        
+        int ret = 0;
+        int i = 0;
+        while(i < s.length()) {
+            if(i + 1 < s.length() && map.get(s.charAt(i)) < map.get(s.charAt(i+1))) {
+                ret += map.get(s.charAt(i+1)) - map.get(s.charAt(i));
+                i += 2;
             }
-            result += curr_val;
-            index++;
-        } 
-        return result;
+            else {
+                ret += map.get(s.charAt(i));
+                i += 1;
+            }
+        }
+        return ret;
     }
-}   
+}
