48aa5ceba8e0ae81ae752eda812c80cfd57e9350
Zhili Hua <zhilihua@Zhili-MacBook-Pro.local>
2017-03-09 14:45:02 -0500

    leetcode 242

---
+++ 242_valid_anagram/Solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) {
+            return false;
+        }
+        char[] s_array = s.toCharArray();
+        Arrays.sort(s_array);
+        char[] t_array = t.toCharArray();
+        Arrays.sort(t_array);
+        
+        for(int i = 0; i < s.length(); i++) {
+            if(s_array[i] != t_array[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
